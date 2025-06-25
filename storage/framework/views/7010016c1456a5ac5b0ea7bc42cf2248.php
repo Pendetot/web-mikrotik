5e9c2b41bb96d8fd3f1e7943523840dd8fb48f40
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-06-25 13:22:55 +0700

    Upload storage/framework/views/7010016c1456a5ac5b0ea7bc42cf2248.php

---
+++ storage/framework/views/7010016c1456a5ac5b0ea7bc42cf2248.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+  <div class="footer-left">
+    Copyright &copy; <?php echo e(date('Y')); ?> <div class="bullet"></div> <?php echo e(config('app.name')); ?>
+
+  </div>
+  <div class="footer-right">
+    Version 1.0
+  </div>
+</footer><?php /**PATH /storage/emulated/0/p/kunyuk/resources/views/layouts/footer.blade.php ENDPATH**/ ?>
