669892f5df2c615070eda6c8ba6bc526371b1989
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-06-25 16:21:30 +0700

    Upload storage/framework/views/02a2840149d76dd3a8847568266b1318.php

---
+++ storage/framework/views/02a2840149d76dd3a8847568266b1318.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>Register &mdash; <?php echo e(config('app.name')); ?></title>
+
+  <!-- General CSS Files -->
+  <link rel="stylesheet" href="<?php echo e(asset('assets/modules/bootstrap/css/bootstrap.min.css')); ?>">
+  <link rel="stylesheet" href="<?php echo e(asset('assets/modules/fontawesome/css/all.min.css')); ?>">
+
+  <!-- CSS Libraries -->
+  <link rel="stylesheet" href="<?php echo e(asset('assets/modules/jquery-selectric/selectric.css')); ?>">
+
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="<?php echo e(asset('assets/css/style.css')); ?>">
+  <link rel="stylesheet" href="<?php echo e(asset('assets/css/components.css')); ?>">
+</head>
+
+<body>
+  <div id="app">
+    <section class="section">
+      <div class="container mt-5">
+        <div class="row">
+          <div class="col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-8 offset-lg-2 col-xl-8 offset-xl-2">
+            <div class="login-brand">
+              <img src="<?php echo e(asset('assets/img/stisla-fill.svg')); ?>" alt="logo" width="100" class="shadow-light rounded-circle">
+            </div>
+
+            <div class="card card-primary">
+              <div class="card-header"><h4>Register</h4></div>
+
+              <div class="card-body">
+                <form method="POST" action="<?php echo e(route('register')); ?>">
+                  <?php echo csrf_field(); ?>
+                  
+                  <div class="form-group">
+                    <label for="name">Full Name</label>
+                    <input id="name" type="text" class="form-control <?php $__errorArgs = ['name'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>" 
+                           name="name" value="<?php echo e(old('name')); ?>" required autofocus>
+                    <?php $__errorArgs = ['name'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                      <div class="invalid-feedback"><?php echo e($message); ?></div>
+                    <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                  </div>
+
+                  <div class="row">
+                    <div class="form-group col-6">
+                      <label for="email">Email</label>
+                      <input id="email" type="email" class="form-control <?php $__errorArgs = ['email'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>" 
+                             name="email" value="<?php echo e(old('email')); ?>">
+                      <?php $__errorArgs = ['email'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                        <div class="invalid-feedback"><?php echo e($message); ?></div>
+                      <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                    </div>
+                    <div class="form-group col-6">
+                      <label for="whatsapp">WhatsApp Number</label>
+                      <input id="whatsapp" type="tel" class="form-control <?php $__errorArgs = ['whatsapp'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>" 
+                             name="whatsapp" value="<?php echo e(old('whatsapp')); ?>" placeholder="628123456789">
+                      <?php $__errorArgs = ['whatsapp'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                        <div class="invalid-feedback"><?php echo e($message); ?></div>
+                      <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                    </div>
+                  </div>
+
+                  <div class="row">
+                    <div class="form-group col-6">
+                      <label for="password" class="d-block">Password</label>
+                      <input id="password" type="password" class="form-control pwstrength <?php $__errorArgs = ['password'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>" 
+                             data-indicator="pwindicator" name="password" required>
+                      <div id="pwindicator" class="pwindicator">
+                        <div class="bar"></div>
+                        <div class="label"></div>
+                      </div>
+                      <?php $__errorArgs = ['password'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                        <div class="invalid-feedback"><?php echo e($message); ?></div>
+                      <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                    </div>
+                    <div class="form-group col-6">
+                      <label for="password_confirmation" class="d-block">Password Confirmation</label>
+                      <input id="password_confirmation" type="password" class="form-control" 
+                             name="password_confirmation" required>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="custom-control custom-checkbox">
+                      <input type="checkbox" name="agree" class="custom-control-input" id="agree" required>
+                      <label class="custom-control-label" for="agree">I agree with the terms and conditions</label>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-lg btn-block">
+                      Register
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+            
+            <div class="mt-5 text-muted text-center">
+              Already have an account? <a href="<?php echo e(route('login')); ?>">Login here</a>
+            </div>
+            <div class="simple-footer">
+              Copyright &copy; <?php echo e(config('app.name')); ?> <?php echo e(date('Y')); ?>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+
+  <!-- General JS Scripts -->
+  <script src="<?php echo e(asset('assets/modules/jquery.min.js')); ?>"></script>
+  <script src="<?php echo e(asset('assets/modules/popper.js')); ?>"></script>
+  <script src="<?php echo e(asset('assets/modules/tooltip.js')); ?>"></script>
+  <script src="<?php echo e(asset('assets/modules/bootstrap/js/bootstrap.min.js')); ?>"></script>
+  <script src="<?php echo e(asset('assets/modules/nicescroll/jquery.nicescroll.min.js')); ?>"></script>
+  <script src="<?php echo e(asset('assets/modules/moment.min.js')); ?>"></script>
+  <script src="<?php echo e(asset('assets/js/stisla.js')); ?>"></script>
+  
+  <!-- JS Libraries -->
+  <script src="<?php echo e(asset('assets/modules/jquery-pwstrength/jquery.pwstrength.min.js')); ?>"></script>
+  <script src="<?php echo e(asset('assets/modules/jquery-selectric/jquery.selectric.min.js')); ?>"></script>
+
+  <!-- Page Specific JS File -->
+  <script src="<?php echo e(asset('assets/js/page/auth-register.js')); ?>"></script>
+  
+  <!-- Template JS File -->
+  <script src="<?php echo e(asset('assets/js/scripts.js')); ?>"></script>
+  <script src="<?php echo e(asset('assets/js/custom.js')); ?>"></script>
+</body>
+</html><?php /**PATH /storage/emulated/0/p/kunyuk/resources/views/auth/register.blade.php ENDPATH**/ ?>
